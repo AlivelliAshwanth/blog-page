a169c4a7179b3054adc98928986b8f8d4f6191b2
Ashwanth2308 <ashwanth@cygnenoircyber.com>
2025-12-31 22:57:50 +0530

    Add fallback Sanity token for Vercel deployment

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -5,5 +5,5 @@ export const client = createClient({
   dataset: 'production',
   useCdn: true,
   apiVersion: '2024-01-01',
-  token: process.env.REACT_APP_SANITY_TOKEN || undefined,
+  token: process.env.REACT_APP_SANITY_TOKEN || 'skclw7xGg0GYqcsnlKdUblbW0DhH3IDLatvOF5x4InjAZsWXLIA3119Vg0SCu7UFZW79jMmTBM4fE5v67RrYIRci6wKQXDXrzyf1SFYYX54uUXNTJKxSfGdJFciyxxKsFjjnqGLyEqY76FxTtIWzEwykEaGHfIkfaZchkk5OAbauflSteB2K',
 });
