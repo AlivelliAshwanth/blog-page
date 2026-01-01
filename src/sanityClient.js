c2d4b2ee9173e84b559323fefaa7cba4dc220bc1
Ashwanth2308 <ashwanth@cygnenoircyber.com>
2026-01-01 23:38:40 +0530

    Hardcode Sanity token for Vercel

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -3,7 +3,7 @@ import { createClient } from '@sanity/client';
 export const client = createClient({
   projectId: 'x9c1zj30',
   dataset: 'production',
-  useCdn: true,
+  useCdn: false,
   apiVersion: '2024-01-01',
-  token: process.env.REACT_APP_SANITY_TOKEN || 'skclw7xGg0GYqcsnlKdUblbW0DhH3IDLatvOF5x4InjAZsWXLIA3119Vg0SCu7UFZW79jMmTBM4fE5v67RrYIRci6wKQXDXrzyf1SFYYX54uUXNTJKxSfGdJFciyxxKsFjjnqGLyEqY76FxTtIWzEwykEaGHfIkfaZchkk5OAbauflSteB2K',
+  token: 'skclw7xGg0GYqcsnlKdUblbW0DhH3IDLatvOF5x4InjAZsWXLIA3119Vg0SCu7UFZW79jMmTBM4fE5v67RrYIRci6wKQXDXrzyf1SFYYX54uUXNTJKxSfGdJFciyxxKsFjjnqGLyEqY76FxTtIWzEwykEaGHfIkfaZchkk5OAbauflSteB2K',
 });
