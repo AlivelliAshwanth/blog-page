15c4078fe3ab81aeb6962b9af6a7191b0c5e4482
Ashwanth2308 <ashwanth@cygnenoircyber.com>
2026-01-02 00:23:59 +0530

    Use stable API version for Vercel compatibility

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -4,7 +4,7 @@ export const client = createClient({
   projectId: 'x9c1zj30',
   dataset: 'production',
   useCdn: false,
-  apiVersion: '2023-05-03',
+  apiVersion: '2021-10-21', // Use stable older version
   token: 'skclw7xGg0GYqcsnlKdUblbW0DhH3IDLatvOF5x4InjAZsWXLIA3119Vg0SCu7UFZW79jMmTBM4fE5v67RrYIRci6wKQXDXrzyf1SFYYX54uUXNTJKxSfGdJFciyxxKsFjjnqGLyEqY76FxTtIWzEwykEaGHfIkfaZchkk5OAbauflSteB2K',
 });
 
